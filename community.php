be4ca1ede8f8ddde105b2bbf30dbfa9c226fe8ba
bahotae <hellobenursal@gmail.com>
2016-11-29 20:04:42 +0800

    community benefits html

---
+++ community.php
@@ -0,0 +1,84 @@
+<?php include 'header.php'; ?>
+<link href="css/page-join.css" rel='stylesheet' type='text/css'>
+
+<!-- hero -->
+<div class="hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1>Community Benefits</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+				<h3>The Right Place at the Right Time</h3>
+				<p>
+					UNOVA Health is located in Lady Lake, Florida just a few miles from The Villages, 
+					the country’s fastest growing retirement community. Approximately 120,000 
+					people currently live in The Villages, and the vast majority are over the age of 65.<br /><br />
+
+					A comprehensive orthopaedic practice, one that combines all medical services 
+					under one roof, will fill a large and growing need in this area.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section section-lightblue">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<div class="white-box">
+					<h3>Many Services, One Location</h3>
+					<p>
+						<img src="images/many-services-one-location.png" class="img-responsive image-align-center" />
+					</p>
+					
+					<p>
+						The UNOVA Health facility will have the highly skilled medical professionals and the latest
+						technology and equipment. Patients will be able to receive all of their orthopaedic care in
+						one place, from diagnostic tests like MRIs and CT scans, to surgery and rehabilitation.<br /><br />
+
+						UNOVA Health will offer fast, easy scheduling and insurance approvals. Extended afterhours 
+						urgent care services will be available. The UNOVA Health facility will also have a community 
+						center where area residents can learn about timely medical topics.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section" id="economy">
+	<div class="container">
+		<div class="row">
+			<h3>Job Creation and Economic Impact</h3>
+			<div class="col-sm-6">
+				<img src="images/economy.png" class="img-responsive" />
+			</div>
+			<div class="col-sm-6">
+				<p>
+					UNOVA Health has done extensive research on the potential 
+					impact of the orthopaedic practice on the local area.<br /><br />
+
+					According to IMPLAN software, a medical office of this type will 
+					have a total economic impact of $23 million per year and will 
+					create 180 jobs in the local community. The annual payroll is 
+					expected to exceed $8 million.<br /><br />
+
+					Additionally, IMPLAN software forecasts the annual total state 
+					and local tax revenue to be $665,000.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php include 'main-cta.php'; ?>
+<?php include 'footer.php'; ?>
