ed3e34606c8840a300202d01cabc8021709fadbb
bahotae <hellobenursal@gmail.com>
2016-11-29 13:07:20 +0800

    homepage HTML only

--- index.php
+++ index.php
@@ -111,12 +111,14 @@
 	
 	
 	<!-- CTA -->
-	<div class="section" id="transforming">
+	<div class="section" id="mainCta">
 		<div class="container">
 			<div class="row">
-				<h3></h3>
-				<div class="col-md-6 col-md-offset-6">
-					
+				<div class="col-lg-12 text-center">
+					<h4>Interested in exploring a future with UNOVA Health?</h4>
+					<img src="images/logo.png" class="img-responsive image-align-center" />
+					<hr />
+					<a href="#" class="btn btn-cta">Contact Us Today</a>
 				</div>
 			</div>
 		</div>
@@ -125,6 +127,59 @@
 	<!-- footer -->
 	<div class="footer">
 		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<h3>Our office</h3>
+					<hr />
+					<p>
+						363 Fennell Blvd<br />
+						Lady Lake, FL 32159
+					</p>
+					<hr />
+					<p>
+						<strong>Email:</strong> <a href="mailto:info@UNOVA-Health.com">info@UNOVA-Health.com</a> <br />
+						<strong>Phone:</strong> 352-775-6348
+					</p>
+				</div>
+				<div class="col-sm-6">
+					<h3>contact us</h3>
+					<form>
+						<div class="form-group">
+							<label class="visible-xs">Name</label>
+							<input type="text" class="form-control input-lg" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<label class="visible-xs">Email address</label>
+							<input type="text" class="form-control input-lg" placeholder="Email address">
+						</div>
+						<div class="form-group">
+							<label class="visible-xs">Phone number</label>
+							<input type="text" class="form-control input-lg" placeholder="Phone number">
+						</div>
+						<div class="form-group">
+							<label class="visible-xs">Message</label>
+							<textarea class="form-control input-lg" rows="3" placeholder="What we will talk about...."></textarea>
+						</div>
+						<button type="submit" class="btn btn-cta btn-block">Send Message</button>
+					</form>
+					
+				</div>
+			</div>
+			
+			<div class="row footer-menu">
+				<div class="col-lg-12 text-center">
+					<ul class="horizontal-menu">
+						<li>Home</li>
+						<li>About Us</li>
+						<li>Services</li>
+						<li>Facility</li>
+						<li>Technology</li>
+						<li>Privacy Policy</li>
+						<li>Terms & Conditions</li>
+					</ul>
+				</div> <!-- end of column -->
+			</div>
+			
 			<div class="row copyright">
 				<div class="col-lg-12 text-center">
 					Copyright &copy; <?php echo date('Y');?> <a href="http://www.unova-health.com">UNOVA Health, Inc.</a>
