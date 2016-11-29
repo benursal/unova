2bed128e26843641d9d4329e4ca682caf00d7226
bahotae <hellobenursal@gmail.com>
2016-11-29 15:07:07 +0800

    services partial

---
+++ services.php
@@ -0,0 +1,107 @@
+<?php include 'header.php'; ?>
+<link href="css/page-services.css" rel='stylesheet' type='text/css'>
+
+<!-- hero -->
+<div class="hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-7">
+				<h1>UNOVA Health Services</h1>
+				<p>
+					UNOVA Health will have the facility, technology and skilled professionals to treat all orthopaedic diseases. Below is a summary of our services.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="section" id="servicesList">
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-sm-6 service-type">
+				<h3>Surgical Specialties</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<ul>
+							<li>Extremity joint replacement</li>
+							<li>Sports medicine and shoulder</li>
+							<li>Foot and ankle</li>
+							<li>Trauma</li>
+							<li>Hip</li>
+							<li>Hand</li>
+							<li>Spine</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<img src="images/operation.png" class="img-responsive" />
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-6 service-type">
+				<h3>Physician Services</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<ul>
+							<li>Extremity joint replacement</li>
+							<li>Sports medicine and shoulder</li>
+							<li>Foot and ankle</li>
+							<li>Trauma</li>
+							<li>Hip</li>
+							<li>Hand</li>
+							<li>Spine</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<img src="images/rehab-icon.png" class="img-responsive" />
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-6 service-type">
+				<h3>In-House (On-site) Ancillary Services</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<ul>
+							<li>Extremity joint replacement</li>
+							<li>Sports medicine and shoulder</li>
+							<li>Foot and ankle</li>
+							<li>Trauma</li>
+							<li>Hip</li>
+							<li>Hand</li>
+							<li>Spine</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<img src="images/radiology.png" class="img-responsive" />
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-sm-6 service-type">
+				<h3>Home Services</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<ul>
+							<li>Extremity joint replacement</li>
+							<li>Sports medicine and shoulder</li>
+							<li>Foot and ankle</li>
+							<li>Trauma</li>
+							<li>Hip</li>
+							<li>Hand</li>
+							<li>Spine</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<img src="images/home-service.png" class="img-responsive" />
+					</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+<?php include 'main-cta.php'; ?>
+<?php include 'footer.php'; ?>
